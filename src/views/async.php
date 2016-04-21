8ee0c1e329a6b8495e9ad8c70fb44df25a5ffe20
Aleksandr <nixomsk@gmail.com>
2016-04-21 11:12:29 +0600

    Update async.php

--- src/views/async.php
+++ src/views/async.php
@@ -7,27 +7,22 @@
 <!-- Yandex.Metrika counter -->
 <script type="text/javascript">
     (function (d, w, c) {
-        (w[c] = w[c] || []).push(function () {
+        (w[c] = w[c] || []).push(function() {
             try {
                 w.yaCounter<?= $counterId ?> = new Ya.Metrika(<?=\yii\helpers\Json::encode($counterParams)?>);
-            } catch (e) {
-            }
+            } catch(e) { }
         });
 
         var n = d.getElementsByTagName("script")[0],
             s = d.createElement("script"),
-            f = function () {
-                n.parentNode.insertBefore(s, n);
-            };
+            f = function () { n.parentNode.insertBefore(s, n); };
         s.type = "text/javascript";
         s.async = true;
-        s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
 
         if (w.opera == "[object Opera]") {
             d.addEventListener("DOMContentLoaded", f, false);
-        } else {
-            f();
-        }
+        } else { f(); }
     })(document, window, "yandex_metrika_callbacks");
 </script>
 <!-- /Yandex.Metrika counter -->
